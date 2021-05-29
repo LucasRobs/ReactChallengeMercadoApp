737dfb4129742937f102525e07c13bc23c3f3158
LucasRobs <lucasrobs@alu.ufc.br>
2021-05-29 19:22:22 -0300

    feat: Completed cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -15,7 +15,26 @@ export default function Cart() {
           <ProductCardSecondary />
           <ProductCardSecondary />
         </div>
-        <div className={styles.rightContainerArea}></div>
+        <div className={styles.rightContainerArea}>
+          <div className={styles.valuesInformation}>
+            <div className={styles.values}>
+              <div>Subtotal:</div> <div>R$5,50</div>
+            </div>
+            <div className={styles.values}>
+              <div>Taxa de Entrega:</div> <div>R$8,50</div>
+            </div>
+            <div className={styles.values}>
+              <div>Economia:</div> <div>- R$3,50</div>
+            </div>
+            <hr />
+            <div className={styles.values}>
+              <div>Total:</div> <div>R$14,00</div>
+            </div>
+          </div>
+          <div className={styles.checkOut}>
+            <button type="button">Finalizar Pedido</button>
+          </div>
+        </div>
       </div>
     </>
   );
