c97b6c353d01ea0daf83586db004dcf42731b62a
LucasRobs <lucasrobs@alu.ufc.br>
2021-05-29 23:55:34 -0300

    Fix: Correction in contexts

---
+++ src/contexts/ProductContext.tsx
@@ -0,0 +1,58 @@
+import { createContext, useState, ReactNode, useEffect } from "react";
+import api from "../services/api";
+
+export interface ProductProps {
+  id: string;
+  order_number: string;
+  stock: number;
+  image: string;
+  description: string;
+  price: number;
+  offer?: number;
+  promotion?: {
+    kind: string;
+    base: number;
+    value: number;
+  };
+}
+
+interface ProductContextData {
+  products: ProductProps[];
+  deliveryTax: number;
+}
+
+interface ProductProviderProps {
+  children: ReactNode;
+}
+
+export const ProductContext = createContext({} as ProductContextData);
+
+export function ProductsProvider({ children }: ProductProviderProps) {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+  const [deliveryTax, setDeliveryTax] = useState<number>();
+
+  async function fetchProducts() {
+    try {
+      const { data } = await api.get("/");
+      setProducts(data.items);
+      setDeliveryTax(data.delivery_tax);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        deliveryTax,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,17 @@ import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import Head from "next/head";
 import "../styles/global.scss";
-import Products from "./Products";
-import Cart from "../pages/Cart";
-
+import { ProductsProvider } from "../contexts/ProductContext";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Supermercado Show</title>
       </Head>
-      <Cart />
+      <ProductsProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ProductsProvider>
     </>
   );
 }
