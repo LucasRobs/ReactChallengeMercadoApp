8ecbcdaba13db581894c31f7d8612a7dfe7277d9
LucasRobs <lucasrobs@alu.ufc.br>
2021-05-31 13:57:49 -0300

    Fix dependency error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import Router from "next/router";
 import Head from "next/head";
-import NProgress from "nprogress";
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { ProductsProvider } from "../contexts/ProductContext";
 import { CartProvider } from "../contexts/CartContext";
 import "../styles/global.scss";
-
-Router.events.on("routeChageStart", (url) => {
-  NProgress.start();
-});
-Router.events.on("routerChangeComplete", () => NProgress.done());
-Router.events.on("routerChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
